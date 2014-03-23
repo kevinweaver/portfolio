76991f049cd371f7ea4ca51148aa669352f0078a
Dean <deanfreekinggalvin@gmail.com>
2014-03-23 17:42:46 -0400

    heroku ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,5 @@
 Portfolio::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
   config.cache_classes = true
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
@@ -27,7 +23,7 @@ Portfolio::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
